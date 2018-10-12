9c1a99e3a476f6678f6de75e89996c73c373ca8c
Fa <ngomfa@gmail.com>
2018-10-12 23:10:23 +0200

    exo5

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+function changeimage(){
+  document.getElementById("image1").src ="assets/img/image1_2.jpg";
+  document.getElementById("image2").src ="assets/img/image2_2.jpg";
+  document.getElementById("image3").src ="assets/img/image3_2.jpg";
+  document.getElementById("image4").src ="assets/img/image4_2.jpg";
+  document.getElementById("image5").src ="assets/img/image5_2.jpg";
+  }
+
+/* Deuxième solution mais dans ce cas mettre (this) dans le html dans les parentheses de changeimage  ie dans onemouseover="changeimage"(this).
+
+function changeimage(image) {
+  image.src = "assets/img/" + image.id + "_2.jpg";
+}*/
